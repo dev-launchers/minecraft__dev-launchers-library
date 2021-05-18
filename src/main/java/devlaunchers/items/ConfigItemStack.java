863fb7eb0e74694df1a4299665e15755cdc4a814
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2021-05-18 13:07:58 +0530

    Update ConfigItemStack.java

--- src/main/java/devlaunchers/items/ConfigItemStack.java
+++ src/main/java/devlaunchers/items/ConfigItemStack.java
@@ -7,6 +7,13 @@ import java.util.Map;
 
 public final class ConfigItemStack extends ItemStack implements ConfigurationSerializable {
 
+  /**
+   * Allows configs to directly give reference to custom ItemStack
+   * items in shared plugin.
+   *
+   * @param args Configuration class map containing keys and values
+   * @return Returns ItemStack from devLauncherItem parameter
+   */
   public static ItemStack deserialize(Map<String, Object> args) {
     String itemName = (String) args.get("devLauncherItem");
     int amount = 1;
